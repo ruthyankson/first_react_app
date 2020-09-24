791fd53eafdb115d685fdff04c06e0f900dfaf59
AdwowaRay <ruthyankson357@gmail.com>
2020-09-24 13:43:13 +0000

    Using Fragment instead of Auxiliary

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Person.css';
 
 
@@ -6,14 +7,21 @@ import classes from './Person.css';
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
-        return [
-            <div className={classes.person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.hobbies}</p>
-                <input type="text" onChange={this.props.changed} 
+        return (
+            <Fragment>
+                <p onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old!
+                </p>,
+                
+                <p>{this.props.children}</p>,
+
+                <input 
+                    key="i3"
+                    type="text" 
+                    onChange={this.props.changed} 
                     value={this.props.name}/>
-            </div>
-        ];
+            </Fragment>
+        );
     }
     
 
